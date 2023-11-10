ca59828ab6da177b8c8f9df791abf0d33d293d8d
PayalSharma2023 <payalsharmareal@gmail.com>
2023-11-10 23:05:08 +0530

    new
    Signed-off-by: PayalSharma2023 <payalsharmareal@gmail.com>

--- node_crash_course/express.js
+++ node_crash_course/express.js
@@ -10,15 +10,21 @@ app.set('view engine', "ejs");
 app.listen(3000);
 
 app.get('/', (req,res) => {
+    const blogs = [
+        {title: 'yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Nario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'how to defeat browser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
 // res.send('<p> home page </p>');
     //res.sendFile('./views/index.html', {root: __dirname});
-    res.render('index');
-
+    res.render('index', { title: "Home", blogs });
 });
+
+
 app.get('/about', (req,res) => {
    // res.sendFile('./views/about.html', {root: __dirname});
     //res.send('<p> About page </p>');
-    res.render("about");
+    res.render("about", {title: "about page"});
 });
 
 //redirects
@@ -26,8 +32,11 @@ app.get('/about-us', (req, res) => {
     res.redirect('/about');
 })
 
+app.get('/blogs/create', (req, res) => {
+    res.render("create", {title: "create new blog"});
+})
 //404 page
 app.use((req, res) => {
    // res.sendFile('./views/404.html', {root: __dirname})
-   res.status(404).render('404');
+   res.status(404).render('404', {title: "404"});
 });
