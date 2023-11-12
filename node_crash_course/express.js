bd36e6512b8e1a31c9a433ba6754cbe1e26eb26d
PayalSharma2023 <payalsharmareal@gmail.com>
2023-11-12 17:11:23 +0530

    updates
    Signed-off-by: PayalSharma2023 <payalsharmareal@gmail.com>

--- node_crash_course/express.js
+++ node_crash_course/express.js
@@ -5,32 +5,79 @@ const app = express();
 const morgan = require("morgan");
 const mongoose = require('mongoose')
 const mongodb = require('mongodb');
+const Blog = require('./models/blog.js');
 const dbURI = "mongodb+srv://peacko:peackopeacko1312@peacko.bl5glnj.mongodb.net//"
-mongoose.connect(dbURI);
+
+
+mongoose.connect(dbURI)
+    .then((result) => {
+        app.listen(3000);
+        // console.log('connected to db')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 //register view engine
 app.set('view engine', "ejs");
 
 //listen for request
-app.listen(3000);
+
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
+//mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'new blog',
+//         snippet: 'about my new blog',
+//         body: 'more about my new blog'
+//     });
+
+//     blog.save()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+//     });
+
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//      })
+// })
+
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById()
+//     .then((result) => {
+//         res.send(result)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+// })
+
 
 app.post('/blogs', (req, res)=>{
     console.log(req.body);
-    // const blog = new blog(req.body);
+    const blog = new Blog(req.body);
     
-    // blog.save()
-    //     .then((result)=>{
-    //         res.redirect('/bogs');
-    //     })
-    //     .catch((err) => {
-    //         console.log(err)  
-    //     })
+    blog.save()
+    .then((result)=>{
+        res.redirect('/bogs');
+    })
+    .catch((err) => {
+        console.log(err)  
+    })
 
 });
 
@@ -43,9 +90,9 @@ app.use((req, res, next) => {
     next();
 });
 
-
-
+//routes
 app.get('/', (req,res) => {
+   // res.redirect('/blogs');
     const blogs = [
         {title: 'yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
         {title: 'Nario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
@@ -54,7 +101,20 @@ app.get('/', (req,res) => {
 // res.send('<p> home page </p>');
     //res.sendFile('./views/index.html', {root: __dirname});
     res.render('index', { title: "Home", blogs });
+
 });
+
+// blog routes
+// app.get('/blogs' , (req, res) => {
+//     Blog.find().sort({createdAt : -1})
+//     .then((result) => {
+//         res.render('index', { title : 'all blogs', blogs: result})
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+
+// })
 
 app.use((req, res, next) => {
     console.log('in the next middleware');
